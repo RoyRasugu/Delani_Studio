4712ea6b04a0331376e8f5f290e2bab381387aaa
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-07-27 09:35:04 +0300

    change the message on sumbit button

--- js/contact_me.js
+++ js/contact_me.js
@@ -30,10 +30,8 @@ $(function () {
           $('#success').html("<div class='alert alert-success'>");
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been received. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
+          $('#success > .alert-success').append($("<strong>").text(firstName + ", we have received your message. Thank you for reaching out to us."));
+          $('#success > .alert-success').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
         },
